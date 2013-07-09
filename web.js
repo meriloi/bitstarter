5634821aafd3faf44e3837b85c82f1b0639343d3
Ivar Merilo <ivar@gateme.com>
2013-07-09 21:04:11 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8",0,27));
+    var index = fs.readFileSync('./index.html','utf8')
+    response.send(index);
 });
 
 var port = process.env.PORT || 5000;
